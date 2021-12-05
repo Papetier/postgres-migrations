8282c1e16f08653ef14bd99b7e9ce0b49f6bfad6
Paul Landázuri <landazuri.p@gmail.com>
2021-12-05 14:25:34 +0000

    feat: Categories for arXiv

--- migrations/000002_scraper.up.sql
+++ migrations/000002_scraper.up.sql
@@ -17,16 +17,19 @@ CREATE TABLE websites
 (
     id            SERIAL PRIMARY KEY,
 
+    category_list TEXT,
     domain_list   TEXT NOT NULL,
     init_url_list TEXT,
     name          TEXT UNIQUE
 );
-COMMENT ON COLUMN websites.init_url_list IS 'comma-separated list of initial urls';
+COMMENT ON COLUMN websites.category_list IS 'comma-separated list of categories to scrape';
 COMMENT ON COLUMN websites.domain_list IS 'comma-separated list of allowed domains';
+COMMENT ON COLUMN websites.init_url_list IS 'comma-separated list of initial urls';
 
-INSERT INTO websites(name, domain_list, init_url_list)
+INSERT INTO websites(name, category_list, domain_list, init_url_list)
 VALUES ('arXiv',
+        'cs.AI,cs.AR,cs.CC,cs.CE,cs.CG,cs.CL,cs.CR,cs.CV,cs.CY,cs.DB,cs.DC,cs.DL,cs.DM,cs.DS,cs.ET,cs.FL,cs.GL,cs.GR,cs.GT,cs.HC,cs.IR,cs.IT,cs.LG,cs.LO,cs.MA,cs.MM,cs.MS,cs.NA,cs.NE,cs.NI',
         'arxiv.org,export.arxiv.org',
-        'http://export.arxiv.org/api/query?id_list=cs/9901002v1%2C0904.2860v1');
+        '');
 
 COMMIT;
