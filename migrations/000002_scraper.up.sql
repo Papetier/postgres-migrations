fe2b1de50fe7129ee5c3e7423bdd351e8e21c1b6
Paul Landázuri <landazuri.p@gmail.com>
2021-12-12 12:24:22 +0000

    fix: Remove category_list & init_url_list from website table

--- migrations/000002_scraper.up.sql
+++ migrations/000002_scraper.up.sql
@@ -17,19 +17,13 @@ CREATE TABLE websites
 (
     id            SERIAL PRIMARY KEY,
 
-    category_list TEXT,
     domain_list   TEXT NOT NULL,
-    init_url_list TEXT,
     name          TEXT UNIQUE
 );
-COMMENT ON COLUMN websites.category_list IS 'comma-separated list of categories to scrape';
 COMMENT ON COLUMN websites.domain_list IS 'comma-separated list of allowed domains';
-COMMENT ON COLUMN websites.init_url_list IS 'comma-separated list of initial urls';
 
-INSERT INTO websites(name, category_list, domain_list, init_url_list)
+INSERT INTO websites(name, domain_list)
 VALUES ('arXiv',
-        'cs.AI,cs.AR,cs.CC,cs.CE,cs.CG,cs.CL,cs.CR,cs.CV,cs.CY,cs.DB,cs.DC,cs.DL,cs.DM,cs.DS,cs.ET,cs.FL,cs.GL,cs.GR,cs.GT,cs.HC,cs.IR,cs.IT,cs.LG,cs.LO,cs.MA,cs.MM,cs.MS,cs.NA,cs.NE,cs.NI,cs.OH,cs.OS,cs.PF,cs.PL,cs.RO,cs.SC,cs.SD,cs.SE,cs.SI,cs.SY',
-        'arxiv.org,export.arxiv.org',
-        '');
+        'arxiv.org,export.arxiv.org');
 
 COMMIT;
